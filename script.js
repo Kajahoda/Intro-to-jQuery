762e8d7c9a7dcdd590df63d8d127323a75364706
Kajahoda <karlicekhav@gmail.com>
2021-08-22 15:15:35 +0200

    changed text with javascript if number is bigger or smaller than 100

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let word = "cool";
 if (color = "purple") {
     $('.red').css({'background' : 'purple'})
 };
+if (number>100) {
+    $('.yellow').text("whoah, that's a big number.");
+}
+else
+{
+    $('.yellow').text("just a regular number, please.");
+}
+
